56d6702555396f415052782f70373752b0d3cb38
Tyler Ruotolo <tylerruotolotech@gmail.com>
2020-06-21 13:06:54 -0400

    v2.0

--- converter.js
+++ converter.js
@@ -1,4 +1,4 @@
-var version = "1.8";
+var version = "2.0";
 var versiontext = document.getElementById("versionnumber");
 var views = document.getElementById("views");
 var spectrumaxis = document.getElementById("politicalaxis");
